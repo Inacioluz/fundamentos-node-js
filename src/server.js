1ae746a7d9970948a76358d4a7b8b70bc60e7fc9
Inacio Silva <joseinaciolds@gmail.com>
2023-09-07 11:08:25 -0300

    implementação de try catch

---
+++ src/server.js
@@ -0,0 +1,45 @@
+import http from 'node:http'
+
+
+const users = []
+
+const server = http.createServer(async(req, res) => {
+    const { method, url } = req 
+
+
+    const buffer = []
+
+for await (const chunk of req) {
+    buffer.push(chunk)
+}
+
+try {
+    req.body = JSON.parse(Buffer.concat(buffer).toString())
+} catch {
+    req.body = null
+}
+
+    if ( method === 'GET' && url === '/users') {
+        return res
+        .setHeader('Content-Type', 'application/json')
+        .end(JSON.stringify(users))
+    }
+
+    if ( method === 'POST' && url === '/users' ) {
+
+        const { name, email } = req.body
+
+        users.push({
+            id: 1,
+            name ,
+            email,
+        })
+
+        return res.writeHead(201).end()
+    }
+   
+
+    return res.writeHead(404).end('Not found');
+})
+
+server.listen(3333)
